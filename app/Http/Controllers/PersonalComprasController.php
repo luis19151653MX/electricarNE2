a7d9d335c5f2f8f36ffbce73a4d8a6afbc647fa5
mascorroSaul <smascorro2001@gmail.com>
2023-03-01 11:16:36 -0600

    Migraciones Saúl

---
+++ app/Http/Controllers/PersonalComprasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\personal_compras;
+use Illuminate\Http\Request;
+
+class PersonalComprasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\personal_compras  $personal_compras
+     * @return \Illuminate\Http\Response
+     */
+    public function show(personal_compras $personal_compras)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\personal_compras  $personal_compras
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(personal_compras $personal_compras)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\personal_compras  $personal_compras
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, personal_compras $personal_compras)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\personal_compras  $personal_compras
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(personal_compras $personal_compras)
+    {
+        //
+    }
+}
