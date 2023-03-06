c07e3705f372b1adededc697ee6e5600df6e37a8
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-03-05 18:30:20 -0600

    blur

--- public/js/app.js
+++ public/js/app.js
@@ -16638,7 +16638,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, ".navbar-blur {\r\n    background-image: linear-gradient(to bottom, #97d8cd 0%, rgba(255, 255, 255, 0.8) 100%);\r\n    -webkit-backdrop-filter: blur(5px);\r\n            backdrop-filter: blur(5px);\r\n}", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, ".navbar-blur {\r\n    background-image: linear-gradient(to bottom, #d6ece9 0%, rgba(255, 255, 255, 0.8) 100%);\r\n    -webkit-backdrop-filter: blur(5px);\r\n            backdrop-filter: blur(5px);\r\n}", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
