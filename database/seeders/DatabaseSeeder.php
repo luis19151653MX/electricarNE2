9d2c0d87a2b41c38a32b630a2baccca8db9f1851
Yairch13 <yairchavez136va@gmail.com>
2023-03-06 10:46:23 -0600

    lista carritos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         
-        $this->call(ProductosSeeder::class);
+      // $this->call(ProductosSeeder::class);
+        
         $this->call(UsuariosSeeder::class);
-        $this->call(UsuariosSeeder::class);
+        $this->call(Carritos_comprasSeeder::class);
+        $this->call(Lista_carritosSeeder::class);
     }
 }
