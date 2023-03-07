587f39359b9ea27e49b873cdbc3af56723392b19
Yairch13 <yairchavez136va@gmail.com>
2023-03-07 09:53:55 -0600

    los amo

--- database/seeders/PersonalVentasSeeder.php
+++ database/seeders/PersonalVentasSeeder.php
@@ -24,22 +24,22 @@ class PersonalVentasSeeder extends Seeder
         $p1->direccion = "1033 Mountain Drive, Gotham City";
         $p1->save();
 
-        $p1 = new personal_ventas();
-        $p1->id = 2;
-        $p1->nombre = "Yair Chávez";
-        $p1->telefono = "4491871289";
-        $p1->contraseña = "nsuns4xd";
-        $p1->email = "yajuu@gmail.com";
-        $p1->direccion = "1082 Clinthon Hill, Metropolis";
-        $p1->save();
+        $p2 = new personal_ventas();
+        $p2->id = 2;
+        $p2->nombre = "Yair Chávez";
+        $p2->telefono = "4491871289";
+        $p2->contraseña = "nsuns4xd";
+        $p2->email = "yajuu@gmail.com";
+        $p2->direccion = "1082 Clinthon Hill, Metropolis";
+        $p2->save();
 
-        $p1 = new personal_ventas();
-        $p1->id = 3;
-        $p1->nombre = "El piojo Alvarado";
-        $p1->telefono = "4491981765";
-        $p1->contraseña = "nsuns4xd";
-        $p1->email = "elpio_jito@gmail.com";
-        $p1->direccion = "109 SoyElRatón, Sinaloa";
-        $p1->save();
+        $p3 = new personal_ventas();
+        $p3->id = 3;
+        $p3->nombre = "El piojo Alvarado";
+        $p3->telefono = "4491981765";
+        $p3->contraseña = "nsuns4xd";
+        $p3->email = "elpio_jito@gmail.com";
+        $p3->direccion = "109 SoyElRatón, Sinaloa";
+        $p3->save();
     }
 }
