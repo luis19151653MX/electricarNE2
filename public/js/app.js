d39423204a6b8a4802426b96755f68d193bea7e5
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2023-03-09 11:33:52 -0600

    navbar no funciono el icno para que descolapsara

--- public/js/app.js
+++ public/js/app.js
@@ -11377,6 +11377,20 @@ function Navigation() {
   });
 }
 
+/*
+
+<Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+            <Navbar.Toggle aria-controls="navbarScroll" data-bs-toggle="collapse" data-bs-target="#navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+                <Nav>
+                    <NavLink  eventKey="1" as={Link} to="/">Home</NavLink>
+                    <NavLink  eventKey="2" as={Link} to="/">About</NavLink>
+                    <NavLink  eventKey="3" as={Link} to="/">Contact</NavLink>
+                </Nav>
+            </Navbar.Collapse>     
+        </Navbar>
+*/
+
 /***/ }),
 
 /***/ "./resources/js/components/Paypal.js":
--- resources/js/components/Navigation.js
+++ resources/js/components/Navigation.js
@@ -44,3 +44,17 @@ export default function Navigation() {
 
     )
 }
+
+/*
+
+<Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+            <Navbar.Toggle aria-controls="navbarScroll" data-bs-toggle="collapse" data-bs-target="#navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+                <Nav>
+                    <NavLink  eventKey="1" as={Link} to="/">Home</NavLink>
+                    <NavLink  eventKey="2" as={Link} to="/">About</NavLink>
+                    <NavLink  eventKey="3" as={Link} to="/">Contact</NavLink>
+                </Nav>
+            </Navbar.Collapse>     
+        </Navbar>
+*/
